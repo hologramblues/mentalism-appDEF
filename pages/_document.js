aca75f6bec135f68aa8f0543c5881140a74d6c98
hologramblues <31226118+hologramblues@users.noreply.github.com>
2024-10-27 09:47:35 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,24 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Mentalism" />
+        
+        {/* Permissions pour Web Bluetooth */}
+        <meta http-equiv="origin-trial" content="bluetooth" />
+        <meta http-equiv="feature-policy" content="bluetooth *" />
+        <meta name="bluetooth" content="yes" />
+        
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png" />
+        
+        {/* Meta tags pour forcer le mode standalone */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <body>
         <Main />
